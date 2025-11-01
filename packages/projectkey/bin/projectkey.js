a1abaaf93a3d2aa5fe294959577b9c6e2e9a6d6f
Christopher Zeuch <chriszeuch.cz@gmail.com>
2025-11-01 19:37:41 +0200

    npm package

---
+++ packages/projectkey/bin/projectkey.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+console.log('Project Key - Universal Authentication Orchestrator for Test Automation');
+console.log('Version 0.1.0');
+console.log('Documentation coming soon...');
+
---
+++ packages/projectkey/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'projectkey',
+  version: '0.1.0'
+};
+
